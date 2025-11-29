9581368962002899f7ce24b686dd94530cf88f57
Kehinde Imani <imamikenny27@gmail.com>
2025-11-29 06:12:08 +0100

    route product id

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const authMiddleware = require('../middleware/authMiddleware');
 const productController = require('../controllers/productController');
+
 const router = express.Router();
 
+// Add a new product
 router.post('/add', authMiddleware, productController.addProduct);
+
+// Get all products
 router.get('/', productController.getProducts);
+
+// Get a product by ID
+router.get('/:id', productController.getProductById);
+
+// Update a product by ID
 router.put('/:id', authMiddleware, productController.updateProduct);
+
+// Delete a product by ID
 router.delete('/:id', authMiddleware, productController.deleteProduct);
 
 module.exports = router;
