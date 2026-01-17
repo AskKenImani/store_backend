953bf286e53b04814faf0feca438fb9834cfc453
Kehinde <imamikenny27@gmail.com>
2026-01-17 11:24:25 +0100

    fix date bug

--- models/Order.js
+++ models/Order.js
@@ -1,15 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const orderSchema = mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  products: [
-    {
-      product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-      quantity: { type: Number, default: 1 },
-    }
-  ],
-  totalAmount: { type: Number, required: true },
-  paymentStatus: { type: String, default: 'pending' },
-});
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: Number,
+      },
+    ],
+    totalAmount: Number,
+    paymentStatus: {
+      type: String,
+      enum: ["pending", "completed", "failed"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
 
-module.exports = mongoose.model('Order', orderSchema);
+module.exports = mongoose.model("Order", orderSchema);
